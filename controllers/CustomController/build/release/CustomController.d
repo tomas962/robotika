5977cad0143144a9e014c7b097dbad6d73e2c78f
Egidijus Kutko <egikut@ktu.lt>
2019-12-20 17:29:38 +0200

    costum contr

---
+++ controllers/CustomController/build/release/CustomController.d
@@ -0,0 +1,9 @@
+build/release/CustomController.o: CustomController.c \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h
---
+++ controllers/CustomController/build/release/khepera3_gripper.d
@@ -0,0 +1,9 @@
+build/release/khepera3_gripper.o: khepera3_gripper.c \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h
