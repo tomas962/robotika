eda36ff89bf8b3d8ff55f864227a005b6ddddc22
tomas962 <cctomass@gmail.com>
2019-12-21 18:17:44 +0200

    throw cubes by bolor

--- controllers/ure_can_grasper/build/release/ure_can_grasper.d
+++ controllers/ure_can_grasper/build/release/ure_can_grasper.d
@@ -1,9 +1,9 @@
 build/release/ure_can_grasper.o: ure_can_grasper.c \
- C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
- C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
- C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/position_sensor.h \
- C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
- C:\Users\kutko\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/position_sensor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/camera.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/camera_recognition_object.h
--- controllers/ure_can_grasper/ure_can_grasper.c
+++ controllers/ure_can_grasper/ure_can_grasper.c
@@ -26,6 +26,35 @@
 #define TIME_STEP 32
 
 enum State { WAITING, GRASPING, ROTATING, RELEASING, ROTATING_BACK };
+enum BLOB_TYPE { RED, GREEN, BLUE, NONE };
+
+enum BLOB_TYPE get_image_color(WbDeviceTag camera, int width, int height){
+    const unsigned char *image = NULL;
+    image = wb_camera_get_image(camera);
+
+    enum BLOB_TYPE current_blob;
+    int red = 0;
+    int blue = 0;
+    int green = 0;
+    for (int i = width / 3; i < 2 * width / 3; i++) {
+        for (int j = height / 2; j < 3 * height / 4; j++) {
+            red += wb_camera_image_get_red(image, width, i, j);
+            blue += wb_camera_image_get_blue(image, width, i, j);
+            green += wb_camera_image_get_green(image, width, i, j);
+        }
+    }
+
+    if ((red > 3 * green) && (red > 3 * blue))
+        current_blob = RED;
+      else if ((green > 3 * red) && (green > 3 * blue))
+        current_blob = GREEN;
+      else if ((blue > 3 * red) && (blue > 3 * green))
+        current_blob = BLUE;
+      else
+        current_blob = NONE;
+
+    return current_blob;
+}
 
 int main(int argc, char **argv) {
   wb_robot_init();
@@ -33,6 +62,7 @@ int main(int argc, char **argv) {
   
   int counter = 0, i = 0;
   int state = WAITING;
+  enum BLOB_TYPE block_color = NONE;
   const double red_box_target_positions[] = {-1.88, -2.14, -2.38, -1.51};
   const double green_box_target_positions[] = {-0.5, -2, -2, 0};
   const double blue_box_target_positions[] = {-2.5, 0, -3, 0};
@@ -68,7 +98,6 @@ int main(int argc, char **argv) {
   int height = wb_camera_get_height(camera);
   printf("height: %d, width: %d \n", height, width);
 
-
   while (wb_robot_step(TIME_STEP) != -1) {
     if (counter <= 0) {
       switch (state) {
@@ -80,14 +109,30 @@ int main(int argc, char **argv) {
             state = GRASPING;
             counter = 8;
             printf("Grasping can\n");
+            printf("Getting color\n");
+            block_color = get_image_color(camera, width, height);
             for (i = 0; i < 3; ++i)
               wb_motor_set_position(hand_motors[i], 0.85);
           }
           break;
         case GRASPING:
-          for (i = 0; i < 4; ++i)
-            wb_motor_set_position(ur_motors[i], blue_box_target_positions[i]);
-          printf("Rotating arm\n");
+          switch (block_color)
+            {
+            case RED:
+              for (i = 0; i < 4; ++i)
+                wb_motor_set_position(ur_motors[i], red_box_target_positions[i]);
+              break;
+            case GREEN:
+              for (i = 0; i < 4; ++i)
+                wb_motor_set_position(ur_motors[i], green_box_target_positions[i]);
+              break;
+            case BLUE:
+              for (i = 0; i < 4; ++i)
+                wb_motor_set_position(ur_motors[i], blue_box_target_positions[i]);
+              break;
+            default:
+              break;
+            }
           state = ROTATING;
           break;
         case ROTATING:
